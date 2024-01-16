cbeb0f5d66f073ba3a6a41845469bb3c2c9f05f5
Oshioke-Salaki <salaki1902@gmail.com>
2024-01-16 15:27:47 +0100

    first commit

--- src/components/NoScroll.jsx
+++ src/components/NoScroll.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 function NoScroll({ children }) {
-  return <div className="!h-screen !overflow-hidden">{children}</div>;
+  return <div className="!h-[100dvh] !overflow-hidden">{children}</div>;
 }
 
 export default NoScroll;
